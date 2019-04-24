db01ded42fd58afa758d7c3bea1c9bb4acd882dd
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 02:35:16 -0700

    created 2 new variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+//Assigned 2 new string variables 
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName ="a";
+var myLastName = "b";
