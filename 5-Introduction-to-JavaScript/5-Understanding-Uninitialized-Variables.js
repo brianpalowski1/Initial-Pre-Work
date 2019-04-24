dd481808fd58776e102216ecdf65faf963cf9627
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 01:20:29 -0700

    Updated and re assigned variables

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,13 @@
+//Variables are initialy undefined when not assigning a value to it.
+
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c ="I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
