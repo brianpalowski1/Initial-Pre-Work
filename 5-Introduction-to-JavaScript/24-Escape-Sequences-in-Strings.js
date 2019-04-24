8575d77248f2a70c17ee4172f702ea1935f30ca6
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 03:10:48 -0700

    Updated var with escape characters

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,11 @@
+// With js there is diffrent ways to code to escape for example
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \r	carriage return
+// \t	tab
+// \b	backspace
+// \f	form feed
+
+var myStr="FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
