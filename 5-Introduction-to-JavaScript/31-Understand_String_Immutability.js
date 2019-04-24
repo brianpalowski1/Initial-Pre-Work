4a9534432f31d99f70caadb9aca501da014224ae
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 04:08:46 -0700

    Updated string with new string value

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// In this section the variable needed to be assigned with a diffrent string
+
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr ="Hello World" // Fix Me
