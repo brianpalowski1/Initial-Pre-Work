70fae2d7baf952aab81e321158fd1896db5bbcbf
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 03:58:52 -0700

    variable now has value of first letter

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,15 @@
+//With bracket notation you can specificaly chose a index within a string
+
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
