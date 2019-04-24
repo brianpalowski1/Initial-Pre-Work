8ba6fb6b98d4ce16954e84ed7527698769a7ff7e
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 04:13:13 -0700

    updated value of variable thirdLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//In this section I had to use bracket notation to select the letter v from the string.
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
