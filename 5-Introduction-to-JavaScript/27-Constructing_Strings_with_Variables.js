41cbb5b1365c8886be9118d29efe4bf05f05cfc9
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 03:31:56 -0700

    Updated var with concat

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+//In this section I created a var and then passed it into another var with concat
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "abc";
+var myStr= "My name is "+ myName+ " and I am well!";
