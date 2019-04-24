dadab746ca980f0ad6a1fc3cee1acb7ce3564d07
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 04:15:43 -0700

    value of var lastLetterOfLastName now last index

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// When selecting the last index of a string its best to use bracket notation by example: test[test.length-1]
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length-1];
