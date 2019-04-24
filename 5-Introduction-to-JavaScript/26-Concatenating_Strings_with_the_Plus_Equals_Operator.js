cb7e76b012800a9d1f67610c68ebdc889b65cc73
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 03:21:52 -0700

    Updated var with concat operator

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// In this section I concated with += operator
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr+= "This is the second sentence."
