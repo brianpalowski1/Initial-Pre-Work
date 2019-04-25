ceba38e2df0b89cca22523cfc4e459d62d0a1e85
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 00:45:31 -0700

    function returns outside or inside

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+// This section goes over The logical or operator (||) returns true if either of the operands is true. Otherwise, it returns false.
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val<10 ) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
