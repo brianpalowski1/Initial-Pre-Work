e23074d6889b40cc42827237ea1a2b24dcdef437
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 21:40:48 -0700

    undated array with pop

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+//This section went over using a menthod called pop that will remove a value at the end of an array
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
