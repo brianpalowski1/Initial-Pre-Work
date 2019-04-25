838472d4ba32afb59c9dcc33c19e1fcd9e47ea3f
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 03:45:00 -0700

    created a while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+//This section went over a while loop and while the statemnet remains true the while loop will continue until it becomes false;
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i=0;
+while(i<5){
+  myArray.push(i);
+  i++;
+}
