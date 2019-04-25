3ef3dcfd0ec8fc5448182b962bd038ba98dde662
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 00:31:38 -0700

    function returns a result of less that operator

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,16 @@
+// The less than operator (<) compares the values of two numbers.
+
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
