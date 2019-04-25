50e1faa29748c9268d17095515cfd9f4a2f57ab1
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 22:16:36 -0700

    passed in 2 arguments to function

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+//In this section  arguments were passed in the function using parameters defined in the function that act as placeholder.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a,b){
+console.log(a+b);
+}
+functionWithArgs(1,2);
