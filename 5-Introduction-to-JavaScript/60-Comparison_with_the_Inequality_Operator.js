cf1100e2b2831dbf13cc5a2736f168fe0f3f8d27
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 00:14:51 -0700

    Updated the function with strict inequality

--- 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -1,4 +1,4 @@
-// This section went over the inequality operator and how it means "Not Equal" and returns false where equality would return true and vice versa.
+// This section went over the inequality operator and how it means "Not Equal" and returns false where equality would return true and vice versa. the inequality operator will convert data types of values while comparing.
 
 // Setup
 function testNotEqual(val) {
