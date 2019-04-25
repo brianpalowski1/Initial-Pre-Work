3ef14068d4a30602ecab95bf3c924565c755ffa9
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 21:05:04 -0700

    Create a new array

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,9 @@
+//This section went over an array to store data and how to decalare it
+
+
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["test",0];
