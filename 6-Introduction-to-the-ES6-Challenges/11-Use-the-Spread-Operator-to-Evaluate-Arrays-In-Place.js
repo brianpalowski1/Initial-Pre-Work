01ec44473f75a36272f4fd76df5fe386c6fe779d
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 06:42:31 -0700

    Updated with spread operator

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,10 @@
+//The spread operator makes this syntax much better to read and maintain.
+
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
