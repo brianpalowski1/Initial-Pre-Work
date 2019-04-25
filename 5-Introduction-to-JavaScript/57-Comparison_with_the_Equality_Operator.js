7b1ff6ff5de24fc968c9bf9ddbd287e2202c9dfa
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 23:56:12 -0700

    this function uses a equality operator

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+//This section goes over the equality operator compares two values and returns true if they're equivalent or false if they are not.
+
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
