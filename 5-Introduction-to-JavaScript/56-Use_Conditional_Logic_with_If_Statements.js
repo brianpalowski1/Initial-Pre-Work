03438871466e0fc2499e0c43a8c84e150cc39738
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 23:53:19 -0700

    function returns a conditional

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,25 @@
+ // This section went over  "conditional logic" and how to use an "if statement" which makes a decision in your code
+
+ // Example
+ function ourTrueOrFalse(isItTrue) {
+   if (isItTrue) {
+     return "Yes, it's true";
+   }
+   return "No, it's false";
+ }
+
+ // Setup
+ function trueOrFalse(wasThatTrue) {
+
+   // Only change code below this line.
+
+   if(wasThatTrue){
+       return "Yes, that was true"
+   }
+   return "No, that was false"
+   // Only change code above this line.
+
+ }
+
+ // Change this value to test
+ trueOrFalse(true);
