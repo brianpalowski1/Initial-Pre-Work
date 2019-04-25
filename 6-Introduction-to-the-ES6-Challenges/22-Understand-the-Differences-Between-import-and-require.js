852e16746cb9f6e1c9f93f92a4333e8676dc1931
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 07:30:27 -0700

    import file

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+// import statment ES6 gives us a very handy tool known as import. With it, we can choose which parts of a module or file to load into a given file, saving time and memory.
+
+"use strict";
+import {capitalizeString} from "string_functions"
+
+  capitalizeString("hello!");
