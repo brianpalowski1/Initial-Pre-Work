cb84212f2e752fbd198e5fcd007e9a3083bc13b9
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 04:07:07 -0700

    For loop increases by 3

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+// In this for loop I will incrament and push the i to arry by threes
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for(var i=1; i< 10; i+=2){
+  myArray.push(i)
+}
