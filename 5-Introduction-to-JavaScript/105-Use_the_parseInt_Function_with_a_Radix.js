07a2a9ed3f5529f216123ebb792229f22fc4a6e4
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 05:10:56 -0700

    Updated function parse int with radix

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,8 @@
+// parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+
+
+function convertToInteger(str) {
+  return parseInt(str,2);
+}
+
+convertToInteger("10011");
