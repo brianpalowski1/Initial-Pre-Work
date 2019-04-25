f19dfe86f3307a8911ebabb6d2ffbebb721009cb
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 22:45:11 -0700

    Update with return

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+// This section went into the use of a return statment inside of a function that will send a value back out of a function
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num){
+  return num* 5;
+}
+
+
+console.log(minusSeven(10));
