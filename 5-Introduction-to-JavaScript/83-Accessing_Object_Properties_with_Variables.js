44c9bfc14a9c56a6688bc6e0ea06e946af670376
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 02:27:00 -0700

    used var to access obj with bracket notation

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,14 @@
+// In this section i was able to access properties with variables then passed using bracket notation
+
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
