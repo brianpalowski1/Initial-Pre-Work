60eb3dba7f8496be97fb73e68d3f80a97707a94b
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 03:13:03 -0700

    Added a new obj in array

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+// This section went over adding multiple objects to an array as well as how objects hold data in a property, which has a key-value format.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist":"test",
+    "title": "test2",
+    "release_year": 2000,
+    "formats": [
+      "1",
+      "2"
+    ]
+  }
+];
