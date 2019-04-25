4c2f841fd7f144a5aceb5b6250b63cb7e3219d0f
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 22:31:46 -0700

    Updated func with local scope

---
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -0,0 +1,14 @@
+// Created local scope inisde function local scope only visable inside the function
+
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar;
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
