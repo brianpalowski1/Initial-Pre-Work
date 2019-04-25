090b61ed2f5678ef5de86c1537922cb06dfe6cf0
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 02:14:56 -0700

    Created an object about a dog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// In this section we went over objects and how they  similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "test",
+  "legs": 1,
+  "tails": 1,
+  "friends": ["test","test"]
+};
