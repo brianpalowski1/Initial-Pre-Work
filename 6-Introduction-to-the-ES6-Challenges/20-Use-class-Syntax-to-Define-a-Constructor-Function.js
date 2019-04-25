a41a1d3a5a0118b43354d36df4eba5175c9df18e
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 07:25:40 -0700

    added a class to function

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,18 @@
+
+//ES6 provides a new syntax to help create objects, using the keyword class.
+
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
