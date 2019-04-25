f749a0248f2f05cd3b9ca94318047277c2dd274e
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 01:50:32 -0700

    Fix code with comparison

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// In this section you can simplify a function with simply using a return with a comparison
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
