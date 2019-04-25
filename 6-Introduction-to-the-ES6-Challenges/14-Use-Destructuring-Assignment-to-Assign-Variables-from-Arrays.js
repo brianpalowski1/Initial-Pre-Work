bddee0c7eae79cee719bcec9971aa4eb461185b4
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 07:00:32 -0700

    Swap destructuring function

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,14 @@
+// ES6 makes destructuring arrays as easy as destructuring objects.
+
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  const c = a;
+a = b;
+b = c;
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
