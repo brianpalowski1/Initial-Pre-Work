7e6e793082f57fef3f2653898c8ad018c7991e6e
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 23:48:08 -0700

    function returns true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,11 @@
+// This section went over "Booleans" which are eaither true or false.
+
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
