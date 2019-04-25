a2c48c9b0df1b1247f8050a4b173355f2a793115
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 07:20:16 -0700

    Updated with es6 feature

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,13 @@
+// ES6 allows you to easily defining object literals. If properties and value are the same being passed in through parameters
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
