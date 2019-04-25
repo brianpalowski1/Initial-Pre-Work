8f305318dcb2cd440f6c7d231c8c2f4f1fc6a171
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 01:12:48 -0700

    function has been corrected

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,16 @@
+////  Excersise to set the correct order of logic to return top to bottom 
+
+
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
