1a6cd752081738f59b2cd2121ab13cd380ab1b36
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 04:58:37 -0700

    Updated function

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// Math random generates random numbers
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
