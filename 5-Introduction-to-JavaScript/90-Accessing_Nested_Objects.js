b65e1b089529b6fcd728b248b688ebd92d110f8a
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 03:17:51 -0700

    selected property with bracket notation

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+//When selecting a property with space in it you need to use bracket notation
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
