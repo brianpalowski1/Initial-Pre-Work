4e0295475e2971254995f2bb818150b42b1c93cc
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 07:35:20 -0700

    exported default in file

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+//used to assign all exports to the namespace variable.
+
+"use strict";
+import * as str from "capitalize_strings";
---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,5 @@
+
+// export default this syntax if only one value is being exported from a file.
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
