439a672ca17267409d31fe1892edee7429afb411
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 00:05:42 -0700

    Function now has an inequality operator

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// This section went over the inequality operator and how it means "Not Equal" and returns false where equality would return true and vice versa.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
