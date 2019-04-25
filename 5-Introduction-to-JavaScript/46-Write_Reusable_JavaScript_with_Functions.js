fc079b6dd8ac17782803ee00ac51b3aa9012efad
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 22:07:26 -0700

    Create a new function

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// This section went over the use of functions and how they are used to write resuable code
+
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log('Hi World');
+}
+reusableFunction();
