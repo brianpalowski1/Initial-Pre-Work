d9967005eff7196da4fc5f89b907e9ce98d1de8a
brianpalowski1 <mail@brianpalowski.com>
2019-04-24 23:26:51 -0700

    Global var updated

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// In this section a function does not need to have a return statement. In this case we are only chaning the global var when run.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive(){
+  sum = sum + 5
+}
+
+// Only change code above this line
+var returnedValue = addFive();
