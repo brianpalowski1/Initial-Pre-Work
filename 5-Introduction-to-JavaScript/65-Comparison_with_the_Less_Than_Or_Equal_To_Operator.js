5468f1f65f6ef7f8d83049849d68826f078d3f48
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 00:33:54 -0700

    function return bool with less than or equal to operator

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+// The less than or equal to operator (<=) compares the values of two numbers.If the number to the left is less than or equal to the number to the right, it returns true. If the number on the left is greater than the number on the right, it returns false
+
+
+function testLessOrEqual(val) {
+  if (val<= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
