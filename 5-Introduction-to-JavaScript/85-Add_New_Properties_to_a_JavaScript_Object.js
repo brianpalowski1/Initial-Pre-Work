b6270a18b7b96dee5273441aa9e8caaa600ef235
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 02:39:56 -0700

    update obj with dot notation

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// This section went over on how to add a property to ann object with using dot notation
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "d"
