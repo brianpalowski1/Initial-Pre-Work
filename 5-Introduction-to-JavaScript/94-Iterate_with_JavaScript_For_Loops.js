348b181a6ad0bfa9fe42e7fc2be6a8f3eb4a4136
brianpalowski1 <mail@brianpalowski.com>
2019-04-25 03:58:45 -0700

    In this function a for loop was added to run same code

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// In this section I created a for loop which will execute the same block of code a specified number of times or while a specified condition is true.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
